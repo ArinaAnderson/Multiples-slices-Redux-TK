d47a71d5e6744aed4c4164d6d4d9eab0c4b926d8
Marina Zinchenko <iolsoofuolso@gmail.com>
2024-06-11 16:15:56 -0700

    fix html in CarForm

--- src/components/CarForm.js
+++ src/components/CarForm.js
@@ -3,8 +3,13 @@ import { useSelector, useDispatch } from 'react-redux';
 
 const CarForm = () => {
   const dispatch = useDispatch();
-  const currentName = useSelector((state) => state.form.name);
-  const currentCost = useSelector((state) => state.form.cost);
+  //cconst currentName = useSelector((state) => state.form.name);
+  //cconst currentCost = useSelector((state) => state.form.cost);
+
+  const { name: currentName, cost: currentCost } = useSelector((state) => ({
+    name: state.form.name,
+    cost: state.form.cost,
+  }))
 
   const inputNamesToActionCreators = {
     name: changeName,
@@ -36,13 +41,16 @@ const CarForm = () => {
             <label className="label" htmlFor="name-input">Car Name</label>
             <input className="input is-expanded" onChange={handleInputChange} id="name-input" name="name" type="text" value={currentName} />
           </div>
+          <div className="field">
+            <label htmlFor="cost-input">Car Cost</label>
+            <input onChange={handleInputChange} id="cost-input" name="cost" type="text" value={currentCost} />
+          </div>
+          <div className="field">
+            <button type="submit" className="button is-link">Submit</button>
+          </div>
         </div>
         
-        <div>
-          <label htmlFor="cost-input">Car Cost</label>
-          <input onChange={handleInputChange} id="cost-input" name="cost" type="text" value={currentCost} />
-        </div>
-        <button type="submit">Submit</button>
+        
       </form>
     </section>);
 }
